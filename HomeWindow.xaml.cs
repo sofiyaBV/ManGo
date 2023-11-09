bf73af56b1de5caac612e6e9f0a65c25f36dbd4f
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-11-09 17:51:02 +0200

    UserControl как вид панели для каждого окна

---
+++ HomeWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+namespace ManGo
+{
+    /// <summary>
+    /// Логика взаимодействия для HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void LV_popular_manga_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+        //private double previousWidth;
+
+        //private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    if (e.NewSize.Width != previousWidth)
+        //    {
+        //        double newWidth;
+        //        if (I_User.Visibility != Visibility.Collapsed)
+        //        { newWidth  = e.NewSize.Width - 320; }
+        //        else { newWidth  = e.NewSize.Width - 410; }
+
+        //        tb_search.Width = newWidth;
+        //        previousWidth = e.NewSize.Width;
+
+        //    }
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button button = (Button)sender;
+        //    button.ContextMenu.IsOpen = true;
+        //}
+    }
+}
---
+++ Presentation/MySPUserControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManGo.Presentation
+{
+    /// <summary>
+    /// Логика взаимодействия для MySPUserControl.xaml
+    /// </summary>
+    public partial class MySPUserControl : UserControl
+    {
+        public MySPUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private double previousWidth;
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width != previousWidth)
+            {
+                double newWidth;
+                if (I_User.Visibility != Visibility.Collapsed)
+                { newWidth  = e.NewSize.Width - 320; }
+                else { newWidth  = e.NewSize.Width - 410; }
+
+                tb_search.Width = newWidth;
+                previousWidth = e.NewSize.Width;
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.ContextMenu.IsOpen = true;
+        }
+
+        private void ManGo_Click(object sender, MouseButtonEventArgs e)
+        {
+            // Получаем родительское окно
+            Window parentWindow = Window.GetWindow(this);
+
+            // Проверяем, что родительское окно существует и является окном
+            if (parentWindow is Window)
+            {
+                HomeWindow homePage = new HomeWindow();
+
+                if (parentWindow.WindowState == WindowState.Maximized)
+                {
+                    homePage.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    homePage.Width = parentWindow.Width;
+                    homePage.Height = parentWindow.Height;
+                    homePage.Left = parentWindow.Left;
+                    homePage.Top = parentWindow.Top;
+                }
+
+                homePage.Show();
+
+                // Закрываем родительское окно
+                parentWindow.Close();
+            }
+        }
+    }
+}
